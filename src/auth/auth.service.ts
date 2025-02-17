b26c863249d0ff3875e7732d26ddc121678c5017
Gyulo94 <qkrcjfrb112@gmail.com>
2025-02-17 14:12:10 +0900

    JWT 액세스토큰, 리프레시토큰 발급급

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,35 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt';
 import { UserService } from 'src/user/user.service';
 import { LoginDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(dto: LoginDto) {
     const user = await this.validateUser(dto);
-    return user;
+    const payload = {
+      email: user.email,
+      sub: { name: user.name, profileImage: user.profileImage },
+    };
+    return {
+      user,
+      serverTokens: {
+        access_token: await this.jwtService.signAsync(payload, {
+          expiresIn: '1h',
+          secret: process.env.JWT_SECRET_KEY,
+        }),
+        refresh_token: await this.jwtService.signAsync(payload, {
+          expiresIn: '7d',
+          secret: process.env.JWT_REFRESH_TOKEN_KEY,
+        }),
+      },
+    };
   }
 
   async validateUser(dto: LoginDto) {
