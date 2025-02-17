48d78178e474710dd7531f6e73c5b1e37c9bf20e
Gyulo94 <qkrcjfrb112@gmail.com>
2025-02-17 13:46:08 +0900

    유저생성 로직직

---
+++ src/user/user.service.ts
@@ -0,0 +1,23 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(dto: CreateUserDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (user) {
+      throw new ConflictException('해당 유저는 이미 존재합니다.');
+    }
+    const newUser = await this.prisma.user.create({
+      data: { ...dto, password: await hash(dto.password, 10) },
+    });
+    const { password, ...result } = newUser;
+    return result;
+  }
+}
