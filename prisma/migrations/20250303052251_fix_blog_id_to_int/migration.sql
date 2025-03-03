69a373f199cf66be6df100f9846973b77ef7fed9
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-04 03:38:04 +0900

    하위 카테고리 테이블 삭제, 이미지 테이블 추가, 블로그 Id 정수로 변경

---
+++ prisma/migrations/20250303052251_fix_blog_id_to_int/migration.sql
@@ -0,0 +1,54 @@
+/*
+  Warnings:
+
+  - The primary key for the `Blog` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `subCategoryId` on the `Blog` table. All the data in the column will be lost.
+  - The `id` column on the `Blog` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The primary key for the `_BlogToTag` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the `SubCategory` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `categoryId` to the `Blog` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `thumnail` to the `Blog` table without a default value. This is not possible if the table is not empty.
+  - Changed the type of `A` on the `_BlogToTag` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Blog" DROP CONSTRAINT "Blog_subCategoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "SubCategory" DROP CONSTRAINT "SubCategory_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_BlogToTag" DROP CONSTRAINT "_BlogToTag_A_fkey";
+
+-- AlterTable
+ALTER TABLE "Blog" DROP CONSTRAINT "Blog_pkey",
+DROP COLUMN "subCategoryId",
+ADD COLUMN     "categoryId" TEXT NOT NULL,
+ADD COLUMN     "thumnail" TEXT NOT NULL,
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "Blog_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "_BlogToTag" DROP CONSTRAINT "_BlogToTag_AB_pkey",
+DROP COLUMN "A",
+ADD COLUMN     "A" INTEGER NOT NULL,
+ADD CONSTRAINT "_BlogToTag_AB_pkey" PRIMARY KEY ("A", "B");
+
+-- DropTable
+DROP TABLE "SubCategory";
+
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Blog" ADD CONSTRAINT "Blog_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BlogToTag" ADD CONSTRAINT "_BlogToTag_A_fkey" FOREIGN KEY ("A") REFERENCES "Blog"("id") ON DELETE CASCADE ON UPDATE CASCADE;
