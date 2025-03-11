b0771f21d005d681ae59febcffb78c624dbf4547
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-12 08:54:08 +0900

    카테고리 dto 수정

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,7 +1,6 @@
-import { IsString, Min } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
-  @Min(1, { message: '한글자 이상 입력하세요.' })
   name: string;
 }
